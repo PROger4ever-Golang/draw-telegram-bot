d339d08928ec1a636d34ecb1adb9ce3e34e1137e
PROger4ever <PROger4ever@gmail.com>
2017-09-26 12:55:02 +0500

    implement state saving to mongo (with auth)

--- main.go
+++ main.go
@@ -1,308 +1,341 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "math/rand"
-    "os"
-    "regexp"
-    "strings"
-    "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 
-    "gopkg.in/mgo.v2"
-    "github.com/go-telegram-bot-api/telegram-bot-api"
-    tuapi "github.com/PROger4ever/telegramapi"
-    "github.com/PROger4ever/telegramapi/mtproto"
+	tuapi "github.com/PROger4ever/telegramapi"
+	"github.com/PROger4ever/telegramapi/mtproto"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 
-    "bitbucket.org/proger4ever/drawtelegrambot/common"
-    "bitbucket.org/proger4ever/drawtelegrambot/config"
-    "bitbucket.org/proger4ever/drawtelegrambot/telegram/userapi"
-    "gopkg.in/mgo.v2/bson"
-    "strconv"
+	"bitbucket.org/proger4ever/drawtelegrambot/common"
+	"bitbucket.org/proger4ever/drawtelegrambot/config"
+	"bitbucket.org/proger4ever/drawtelegrambot/telegram/userapi"
 )
 
-type StateSetting struct {
-    ID    bson.ObjectId `bson:"_id,omitempty"`
-    Name  string
-    Value *tuapi.State
+type SettingState struct {
+	ID    bson.ObjectId `bson:"_id,omitempty"`
+	Name  string
+	Value *StateSerializable
 }
 
-//type State struct {
-//    PreferredDC int
-//
-//    DCs map[int]*DCState
-//
-//    LoginState    LoginState
-//    PhoneNumber   string
-//    PhoneCodeHash string
-//
-//    UserID    int
-//    FirstName string
-//    LastName  string
-//    Username  string
-//}
+func (ss *SettingState) DecodeValue() *tuapi.State {
+	ser := ss.Value
+	st := &tuapi.State{
+		PreferredDC: ser.PreferredDC,
 
-//func (set *StateSetting) Decode(m *bson.M) {
-//    x := *m
-//
-//    set.ID = x["_id"].(bson.ObjectId)
-//    set.Name = x["name"].(string)
-//
-//    val := x["value"].(bson.M)
-//    set.Value = &tuapi.State{
-//        PreferredDC: val["prefereddc"].(int),
-//
-//        LoginState:    val["loginstate"].(tuapi.LoginState),
-//        PhoneNumber:   val["phonenumber"].(string),
-//        PhoneCodeHash: val["phonecodehash"].(string),
-//
-//        UserID:    val["userid"].(int),
-//        FirstName: val["firstname"].(string),
-//        LastName:  val["lastname"].(string),
-//        Username:  val["username"].(string),
-//    }
-//
-//    dcs := val["dcs"].(bson.M)
-//    for km, vm := range dcs {
-//        kc, err := strconv.Atoi(km)
-//        common.PanicIfError(err, "convert km to kc")
-//
-//        dcm := vm.(bson.M)
-//        dcc := tuapi.DCState{}
-//
-//        dcc.ID = dcm["id"].(int)
-//        primaryAddrM := dcm["primaryaddr"].(bson.M)
-//        dcc.PrimaryAddr = tuapi.Addr{
-//            IP: primaryAddrM["ip"].(string),
-//            Port: primaryAddrM["port"].(int),
-//        }
-//        authM := dcm["auth"].(bson.M)
-//
-//        keyB := authM["key"].(bson.Binary)
-//        serverSaltB := authM["serversalt"].(bson.Binary)
-//        serverSaltS := serverSaltB.Data
-//        serverSalt := [8]byte{}
-//        copy(serverSalt[:], serverSaltS)
-//        dcc.Auth = mtproto.AuthResult {
-//            Key: keyB.Data,
-//            KeyID: authM["keyid"].(uint64),
-//            ServerSalt: serverSalt,
-//            KeyID: authM["timeoffset"].(uint64),
-//        }
-//
-//        set.Value.DCs[kc] =
-//    }
-//}
+		LoginState:    ser.LoginState,
+		PhoneNumber:   ser.PhoneNumber,
+		PhoneCodeHash: ser.PhoneCodeHash,
+
+		UserID:    ser.UserID,
+		FirstName: ser.FirstName,
+		LastName:  ser.LastName,
+		Username:  ser.Username,
+	}
+
+	st.DCs = make(map[int]*tuapi.DCState, len(st.DCs))
+	for _, dc := range ser.DCs {
+		st.DCs[dc.ID] = &tuapi.DCState{
+			ID:          dc.ID,
+			PrimaryAddr: dc.PrimaryAddr,
+			FramerState: dc.FramerState,
+			Auth: mtproto.AuthResult{
+				Key:        dc.Auth.Key,
+				KeyID:      (uint64(dc.Auth.KeyIDHigh) << 32) + uint64(dc.Auth.KeyIDLow),
+				ServerSalt: dc.Auth.ServerSalt,
+				TimeOffset: dc.Auth.TimeOffset,
+				SessionID:  dc.Auth.SessionID,
+			},
+		}
+	}
+	return st
+}
+
+type StateSerializable struct {
+	PreferredDC int
+
+	DCs []*DCState
+
+	LoginState    tuapi.LoginState
+	PhoneNumber   string
+	PhoneCodeHash string
+
+	UserID    int
+	FirstName string
+	LastName  string
+	Username  string
+}
+
+type DCState struct {
+	ID int
+
+	PrimaryAddr tuapi.Addr
+
+	Auth        AuthResult
+	FramerState mtproto.FramerState
+}
+
+type AuthResult struct {
+	Key        []byte
+	KeyIDHigh  uint32
+	KeyIDLow   uint32
+	ServerSalt [8]byte
+	TimeOffset int
+	SessionID  [8]byte
+}
+
+func NewStateSerializable(st *tuapi.State) *StateSerializable {
+	ser := &StateSerializable{
+		PreferredDC: st.PreferredDC,
+
+		LoginState:    st.LoginState,
+		PhoneNumber:   st.PhoneNumber,
+		PhoneCodeHash: st.PhoneCodeHash,
+
+		UserID:    st.UserID,
+		FirstName: st.FirstName,
+		LastName:  st.LastName,
+		Username:  st.Username,
+	}
+
+	ser.DCs = make([]*DCState, len(st.DCs))
+	i := 0
+	for _, dc := range st.DCs {
+		ser.DCs[i] = &DCState{
+			ID:          dc.ID,
+			PrimaryAddr: dc.PrimaryAddr,
+			FramerState: dc.FramerState,
+			Auth: AuthResult{
+				Key:        dc.Auth.Key,
+				KeyIDHigh:  uint32(dc.Auth.KeyID >> 32),
+				KeyIDLow:   uint32(dc.Auth.KeyID),
+				ServerSalt: dc.Auth.ServerSalt,
+				TimeOffset: dc.Auth.TimeOffset,
+				SessionID:  dc.Auth.SessionID,
+			},
+		}
+		i++
+	}
+	return ser
+}
 
 var cmdRegexp = regexp.MustCompile("^/([A-Za-z0-9_-]+)@([A-Za-z0-9_-]+) ?(.*)")
 
 var tool *userapi.Tool
 
 func processCmd(bot *tgbotapi.BotAPI, update tgbotapi.Update, chatID int64, name string, params []string) error {
-    switch name {
-    case "draw":
-        resp := "Starting a new drawing..."
-        msg := tgbotapi.NewMessage(chatID, resp)
-        bot.Send(msg)
+	switch name {
+	case "draw":
+		resp := "Starting a new drawing..."
+		msg := tgbotapi.NewMessage(chatID, resp)
+		bot.Send(msg)
 
-        r, err := tool.MessagesGetFullChat(int(chatID))
-        resp = ""
-        if err == nil {
-            resp = fmt.Sprintf("MessagesGetFullChat result: %q", r)
-        } else {
-            resp = fmt.Sprintf("error: %v", err)
-        }
-        msg = tgbotapi.NewMessage(chatID, resp)
-        _, err = bot.Send(msg)
+		r, err := tool.MessagesGetFullChat(int(chatID))
+		resp = ""
+		if err == nil {
+			resp = fmt.Sprintf("MessagesGetFullChat result: %q", r)
+		} else {
+			resp = fmt.Sprintf("error: %v", err)
+		}
+		msg = tgbotapi.NewMessage(chatID, resp)
+		_, err = bot.Send(msg)
 
-        // membersCount, err := bot.GetChatMembersCount(tgbotapi.ChatConfig{
-        // 	chatID,
-        // 	"",
-        // })
-        // common.PanicIfError(err, "GetChatMembersCount")
+		// membersCount, err := bot.GetChatMembersCount(tgbotapi.ChatConfig{
+		// 	chatID,
+		// 	"",
+		// })
+		// common.PanicIfError(err, "GetChatMembersCount")
 
-        // memberID := rand.Intn(membersCount)
+		// memberID := rand.Intn(membersCount)
 
-        // member, err := bot.GetChatMember(tgbotapi.ChatConfigWithUser{
-        // 	chatID,
-        // 	"",
-        // 	memberID,
-        // })
-        // common.PanicIfError(err, "GetChatMember")
-        // fmt.Printf("member: %v\n", member)
-        return err
-        break
-    case "startLogin":
-        // cmdLine := flag.NewFlagSet("", flag.PanicOnError)
-        // phone := cmdLine.String("phone", "", "")
-        // cmdLine.Parse(params)
+		// member, err := bot.GetChatMember(tgbotapi.ChatConfigWithUser{
+		// 	chatID,
+		// 	"",
+		// 	memberID,
+		// })
+		// common.PanicIfError(err, "GetChatMember")
+		// fmt.Printf("member: %v\n", member)
+		return err
+		break
+	case "startLogin":
+		// cmdLine := flag.NewFlagSet("", flag.PanicOnError)
+		// phone := cmdLine.String("phone", "", "")
+		// cmdLine.Parse(params)
 
-        // fmt.Printf("phone: %v", *phone)
+		// fmt.Printf("phone: %v", *phone)
 
-        if len(params) != 1 {
-            return errors.New("Params for command startLogin are incorrect")
-        }
+		if len(params) != 1 {
+			return errors.New("Params for command startLogin are incorrect")
+		}
 
-        err := tool.StartLogin(params[0])
+		err := tool.StartLogin(params[0])
 
-        resp := ""
-        if err == nil {
-            resp = fmt.Sprintf("/completeLoginWithCode@%v *", bot.Self.UserName)
-        } else {
-            resp = fmt.Sprintf("error: %v", err)
-        }
-        msg := tgbotapi.NewMessage(chatID, resp)
-        _, err = bot.Send(msg)
+		resp := ""
+		if err == nil {
+			resp = fmt.Sprintf("/completeLoginWithCode@%v *", bot.Self.UserName)
+		} else {
+			resp = fmt.Sprintf("error: %v", err)
+		}
+		msg := tgbotapi.NewMessage(chatID, resp)
+		_, err = bot.Send(msg)
 
-        return err
-    case "completeLoginWithCode":
-        if len(params) != 1 {
-            return errors.New("Params for command completeLoginWithCode are incorrect")
-        }
+		return err
+	case "completeLoginWithCode":
+		if len(params) != 1 {
+			return errors.New("Params for command completeLoginWithCode are incorrect")
+		}
 
-        phoneCode := strings.Replace(params[0], "-", "", -1)
-        user, err := tool.CompleteLoginWithCode(phoneCode)
+		phoneCode := strings.Replace(params[0], "-", "", -1)
+		user, err := tool.CompleteLoginWithCode(phoneCode)
 
-        resp := ""
-        if err == nil {
-            resp = fmt.Sprintf("Кажется, мы успешно авторизовались: id %d name <%s %s>\n", user.ID, user.FirstName, user.LastName)
-        } else {
-            resp = fmt.Sprintf("error: %v", err)
-        }
-        msg := tgbotapi.NewMessage(chatID, resp)
-        _, err = bot.Send(msg)
+		resp := ""
+		if err == nil {
+			resp = fmt.Sprintf("Мы успешно авторизовались.\nUserID: %d\nUsername: %s\nName: %s %s", user.ID, user.Username, user.FirstName, user.LastName)
+		} else {
+			resp = fmt.Sprintf("error: %v", err)
+		}
+		msg := tgbotapi.NewMessage(chatID, resp)
+		_, err = bot.Send(msg)
 
-        return err
-    case "panic":
-        panic("panic test")
-    default:
-        fmt.Fprint(os.Stderr, fmt.Errorf("Unknown cmd: %v", name))
+		// tool.Conn.Shutdown()
 
-        resp := "Unknown cmd"
-        msg := tgbotapi.NewMessage(chatID, resp)
-        bot.Send(msg)
-    }
+		return err
+	case "panic":
+		panic("panic test")
+	default:
+		fmt.Fprint(os.Stderr, fmt.Errorf("Unknown cmd: %v", name))
 
-    return nil
+		resp := "Unknown cmd"
+		msg := tgbotapi.NewMessage(chatID, resp)
+		bot.Send(msg)
+	}
+
+	return nil
 }
 
 func processMessage(bot *tgbotapi.BotAPI, update tgbotapi.Update, chatID int64, txt string) error {
-    cmdSubmatches := cmdRegexp.FindStringSubmatch(txt)
+	cmdSubmatches := cmdRegexp.FindStringSubmatch(txt)
 
-    if len(cmdSubmatches) == 0 {
-        return nil
-    }
+	if len(cmdSubmatches) == 0 {
+		return nil
+	}
 
-    cmdName := cmdSubmatches[1]
-    cmdBot := cmdSubmatches[2]
-    cmdParams := strings.Fields(cmdSubmatches[3])
-    if cmdBot != bot.Self.UserName {
-        return nil
-    }
+	cmdName := cmdSubmatches[1]
+	cmdBot := cmdSubmatches[2]
+	cmdParams := strings.Fields(cmdSubmatches[3])
+	if cmdBot != bot.Self.UserName {
+		return nil
+	}
 
-    fmt.Printf("Got cmd for me: %v\nGot params: %q\n", cmdName, cmdParams)
-    return processCmd(bot, update, chatID, cmdName, cmdParams)
+	fmt.Printf("Got cmd for me: %v\nGot params: %q\n", cmdName, cmdParams)
+	return processCmd(bot, update, chatID, cmdName, cmdParams)
 }
 
 func processUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-    defer common.RepairIfError("processing update", update)
+	defer common.RepairIfError("processing update", update)
 
-    var msg *tgbotapi.Message
-    if update.Message != nil {
-        msg = update.Message
-    } else if update.ChannelPost != nil {
-        msg = update.ChannelPost
-    }
+	var msg *tgbotapi.Message
+	if update.Message != nil {
+		msg = update.Message
+	} else if update.ChannelPost != nil {
+		msg = update.ChannelPost
+	}
 
-    if len(msg.Text) > 0 {
-        err := processMessage(bot, update, msg.Chat.ID, msg.Text)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Expected error while processing message: %v\n", err)
-        }
-    }
+	if len(msg.Text) > 0 {
+		err := processMessage(bot, update, msg.Chat.ID, msg.Text)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Expected error while processing message: %v\n", err)
+		}
+	}
 
-    // if msg.NewChatMembers != nil {
+	// if msg.NewChatMembers != nil {
 
-    // }
+	// }
 
-    // if msg.LeftChatMember != nil {
+	// if msg.LeftChatMember != nil {
 
-    // }
+	// }
 }
 
 func main() {
-    rand.NewSource(time.Now().UnixNano())
+	rand.NewSource(time.Now().UnixNano())
 
-    conf, err := config.LoadConfig("config.json")
-    common.PanicIfError(err, "reading/decoding config file")
+	conf, err := config.LoadConfig("config.json")
+	common.PanicIfError(err, "reading/decoding config file")
 
-    //region mongo
-    mongoSession, err := mgo.Dial(fmt.Sprintf("%v:%v", conf.Mongo.Host, conf.Mongo.Port))
-    common.PanicIfError(err, "opening mongoSession")
-    mongoSession.SetMode(mgo.Monotonic, true)
-    defer mongoSession.Close()
-    fmt.Println("MongoSession opened")
+	//region mongo
+	mongoSession, err := mgo.Dial(fmt.Sprintf("%v:%v", conf.Mongo.Host, conf.Mongo.Port))
+	common.PanicIfError(err, "opening mongoSession")
+	mongoSession.SetMode(mgo.Monotonic, true)
+	defer mongoSession.Close()
+	fmt.Println("MongoSession opened")
 
-    state := tuapi.State{}
-    stateSettingM := bson.M{}
-    stateSetting := StateSetting{}
-    err = mongoSession.DB("mazimotaBot").C("settings").Find(bson.M{
-        "name": "state",
-    }).One(&stateSettingM)
-    if err == nil {
-        fmt.Printf("stateSetting: %q", stateSettingM)
-        stateSetting.Decode(&stateSettingM)
-        //state = stateSetting.Value
-        fmt.Println("Bot state loaded from mongo")
-    } else {
-        if err == mgo.ErrNotFound {
-            fmt.Println("Clear state created for bot")
-        } else {
-            common.PanicIfError(err, "loading saved bot state from mongo")
-        }
-    }
-    fmt.Printf("state: %q", state)
-    //endregion
+	state := &tuapi.State{}
+	settingState := SettingState{}
+	err = mongoSession.DB("mazimotaBot").C("settings").Find(bson.M{
+		"name": "state",
+	}).One(&settingState)
+	if err == nil {
+		state = settingState.DecodeValue()
+		//state = stateSetting.Value
+		fmt.Println("Bot state loaded from mongo")
+	} else {
+		if err == mgo.ErrNotFound {
+			fmt.Println("Clear state created for bot")
+		} else {
+			common.PanicIfError(err, "loading saved bot state from mongo")
+		}
+	}
+	fmt.Printf("state: %q", state)
+	//endregion
 
-    //region user api
-    uac := conf.Telegram.UserApi
-    tool = &userapi.Tool{}
-    err = tool.Run(&state, uac.Host, uac.Port, uac.PublicKey, uac.ApiId, uac.ApiHash, 0)
-    common.PanicIfError(err, "connecting to Telegram User API")
-    //fmt.Println(tool)
-    fmt.Println("Connected to Telegram User API.")
-    //endregion
+	//region user api
+	uac := conf.Telegram.UserApi
+	tool = &userapi.Tool{}
+	err = tool.Run(state, uac.Host, uac.Port, uac.PublicKey, uac.ApiId, uac.ApiHash, 3)
+	common.PanicIfError(err, "connecting to Telegram User API")
+	//fmt.Println(tool)
+	fmt.Println("Connected to Telegram User API.")
+	//endregion
 
-    //region bot
-    bac := conf.Telegram.BotApi
-    competeKey := fmt.Sprintf("%v:%v", bac.ID, bac.Key)
-    bot, err := tgbotapi.NewBotAPI(competeKey)
-    common.PanicIfError(err, "creating bot instance")
-    bot.Debug = false
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
-    updates, err := bot.GetUpdatesChan(u)
-    common.PanicIfError(err, "getting updates chan")
-    fmt.Printf("Authorized on bot %s\n", bot.Self.UserName)
-    //endregion
+	//region bot
+	bac := conf.Telegram.BotApi
+	competeKey := fmt.Sprintf("%v:%v", bac.ID, bac.Key)
+	bot, err := tgbotapi.NewBotAPI(competeKey)
+	common.PanicIfError(err, "creating bot instance")
+	bot.Debug = false
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	common.PanicIfError(err, "getting updates chan")
+	fmt.Printf("Authorized on bot %s\n", bot.Self.UserName)
+	//endregion
 
-    for {
-        select {
-        case err = <-tool.ErrCh:
-            common.PanicIfError(err, "working with Telegram User API")
-            break
-        case update := <-updates:
-            processUpdate(bot, update)
-            break
-        case _ = <-tool.StateCh:
-            //_, err := mongoSession.DB("mazimotaBot").C("settings").Upsert(bson.M{
-            //    "name": "state",
-            //}, StateSetting{
-            //    Name:  "state",
-            //    Value: state,
-            //})
-            //common.PanicIfError(err, "saving bot state")
-            //fmt.Printf("save state: %q\n", state)
-        }
-    }
+	for {
+		select {
+		case err = <-tool.ErrCh:
+			common.PanicIfError(err, "working with Telegram User API")
+			break
+		case update := <-updates:
+			processUpdate(bot, update)
+			break
+		case state := <-tool.StateCh:
+			_, err := mongoSession.DB("mazimotaBot").C("settings").Upsert(bson.M{
+				"name": "state",
+			}, SettingState{
+				Name:  "state",
+				Value: NewStateSerializable(&state),
+			})
+			common.PanicIfError(err, "saving bot state")
+			// fmt.Printf("save state: %q\n", state)
+		}
+	}
 }
--- telegram/userapi/userapi.go
+++ telegram/userapi/userapi.go
@@ -1,96 +1,97 @@
 package userapi
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    tuapi "github.com/PROger4ever/telegramapi"
-    "github.com/PROger4ever/telegramapi/mtproto"
-    "log"
-    "errors"
+	"errors"
+	"log"
+
+	tuapi "github.com/PROger4ever/telegramapi"
+	"github.com/PROger4ever/telegramapi/mtproto"
 )
 
 type Tool struct {
-    Conn    *tuapi.Conn
-    State   *tuapi.State
-    StateCh chan tuapi.State
-    ErrCh   chan error
-    readyCh chan bool
+	Conn    *tuapi.Conn
+	State   *tuapi.State
+	StateCh chan tuapi.State
+	ErrCh   chan error
+	readyCh chan bool
 }
 
 func (tool *Tool) HandleConnectionReady() {
-    tool.readyCh <- true
+	tool.readyCh <- true
 }
 func (tool *Tool) HandleStateChanged(newState *tuapi.State) {
-    tool.State = newState
-    tool.StateCh <- *newState
+	tool.State = newState
+	tool.StateCh <- *newState
 }
 
 func (tool *Tool) Run(state *tuapi.State, host string, port int, publicKey string, apiId int, apiHash string, verbose int) error {
-    var err error
+	var err error
 
-    options := tuapi.Options{
-        SeedAddr:  tuapi.Addr{ IP: host, Port: port },
-        PublicKey: publicKey,
-        Verbose:   verbose,
-        APIID:     apiId,
-        APIHash:   apiHash,
-    }
+	options := tuapi.Options{
+		SeedAddr:  tuapi.Addr{IP: host, Port: port},
+		PublicKey: publicKey,
+		Verbose:   verbose,
+		APIID:     apiId,
+		APIHash:   apiHash,
+	}
 
-    tool.StateCh = make(chan tuapi.State, 5)
-    tool.ErrCh = make(chan error, 5)
-    tool.readyCh = make(chan bool, 5)
-    tool.Conn = tuapi.New(options, state, tool)
+	tool.StateCh = make(chan tuapi.State, 5)
+	tool.ErrCh = make(chan error, 5)
+	tool.readyCh = make(chan bool, 5)
+	tool.Conn = tuapi.New(options, state, tool)
 
-    go tool.runProcessing()
+	go tool.runProcessing()
 
-    select {
-    case <-tool.readyCh:
-        return nil
-    case err = <-tool.ErrCh:
-        return err
-    case <-time.After(10 * time.Second):
-        return fmt.Errorf("timeout")
-    }
+	select {
+	case <-tool.readyCh:
+		return nil
+	case err = <-tool.ErrCh:
+		return err
+	case <-time.After(1000 * time.Second):
+		return fmt.Errorf("timeout")
+	}
 }
 
 func (tool *Tool) StartLogin(phoneNumber string) error {
-    return tool.Conn.StartLogin(phoneNumber)
+	return tool.Conn.StartLogin(phoneNumber)
 }
 
 func (tool *Tool) CompleteLoginWithCode(phoneCode string) (*mtproto.TLUser, error) {
-    auth, err := tool.Conn.CompleteLoginWithCode(phoneCode)
-    if err != nil {
-        return nil, err
-    }
+	auth, err := tool.Conn.CompleteLoginWithCode(phoneCode)
+	if err != nil {
+		return nil, err
+	}
 
-    if user, ok := auth.User.(*mtproto.TLUser); ok {
-        return user, nil
-    }
-    return nil, errors.New("can't cast response to TLUser type")
+	if user, ok := auth.User.(*mtproto.TLUser); ok {
+		return user, nil
+	}
+	return nil, errors.New("can't cast response to TLUser type")
 }
 
 func (tool *Tool) MessagesGetFullChat(chatId int) (*mtproto.TLMessagesChatFull, error) {
-    r, err := tool.Conn.Send(&mtproto.TLMessagesGetFullChat{ChatID: chatId})
-    if err != nil {
-        return nil, err
-    }
-    switch r := r.(type) {
-    case *mtproto.TLMessagesChatFull:
-        if tool.Conn.Verbose >= 2 {
-            log.Printf("Got messages.getFullChat response: %v", r)
-        }
-        return r, nil
-    default:
-        return nil, tool.Conn.HandleUnknownReply(r)
-    }
+	r, err := tool.Conn.Send(&mtproto.TLMessagesGetFullChat{ChatID: chatId})
+	if err != nil {
+		return nil, err
+	}
+	switch r := r.(type) {
+	case *mtproto.TLMessagesChatFull:
+		if tool.Conn.Verbose >= 2 {
+			log.Printf("Got messages.getFullChat response: %v", r)
+		}
+		return r, nil
+	default:
+		return nil, tool.Conn.HandleUnknownReply(r)
+	}
 }
 
 func (tool *Tool) runProcessing() {
-    err := tool.Conn.Run()
-    if err != nil {
-        tool.ErrCh <- err
-    }
+	err := tool.Conn.Run()
+	if err != nil {
+		tool.ErrCh <- err
+	}
 }
 
 // tool.Conn.Fail(err)
